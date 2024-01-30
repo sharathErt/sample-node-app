0207802ec8fe24fbfde72083a716989cf3852dd9
sharathErt <sharathepalapally@errortech.in>
2024-01-30 12:47:19 +0530

    node app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = 3000
+
+app.get('/hello', (req, res)=>{
+    res.status(200).json({message: 'hello world!'});
+})
+app.listen(port, ()=> console.log('listening on port ' + port))
