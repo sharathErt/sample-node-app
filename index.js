89cf2be3713f0282df50acbd991e16e34d71ed3a
sharathErt <sharathepalapally@errortech.in>
2024-02-01 11:46:55 +0530

    added login route

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const port = 3000
 
 app.get('/hello', (req, res)=>{
     res.status(200).json({message: 'hello world!'});
+})
+
+app.post('/login', (req, res)=>{
+    // console.log(req.body)
+    const { mobile, password} = req.body;
+    console.log({
+        mobile: mobile ,
+        password: password
+    })
+    const userMobile= 7702145910;
+    const userPassword = "sharath123"
+
+    if(mobile === userMobile && password === userPassword){
+        console.log("login successful")
+        res.status(200).json({message:"login success"})
+    }else{
+        console.log("login failed")
+        res.status(401).json({message:"login failed"});
+    }
 })
 app.listen(port, ()=> console.log('listening on port ' + port))
